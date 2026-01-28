2199900a2b31384281fb6b33ed125460e7bc23be
jbalbero.553703 <j.balbero.553703@umindanao.edu.ph>
2026-01-28 09:54:28 +0800

    sdsdsd

--- lab2/Form1.Designer.cs
+++ lab2/Form1.Designer.cs
@@ -20,20 +20,246 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            button2 = new Button();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            label6 = new Label();
+            button3 = new Button();
+            label7 = new Label();
+            textBox4 = new TextBox();
+            label8 = new Label();
+            button4 = new Button();
+            textBox5 = new TextBox();
+            label9 = new Label();
+            label10 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(81, 127);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(187, 101);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(81, 98);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(81, 80);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 3;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(481, 83);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 7;
+            label3.Text = "label3";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(481, 101);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(587, 104);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 5;
+            label4.Text = "label4";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(481, 130);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 4;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(81, 257);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 15);
+            label5.TabIndex = 11;
+            label5.Text = "label5";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(81, 275);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 10;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(187, 278);
+            label6.Name = "label6";
+            label6.Size = new Size(38, 15);
+            label6.TabIndex = 9;
+            label6.Text = "label6";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(81, 304);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 8;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(472, 222);
+            label7.Name = "label7";
+            label7.Size = new Size(38, 15);
+            label7.TabIndex = 15;
+            label7.Text = "label7";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(472, 278);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 14;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(578, 281);
+            label8.Name = "label8";
+            label8.Size = new Size(38, 15);
+            label8.TabIndex = 13;
+            label8.Text = "label8";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(472, 307);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 12;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(472, 249);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 16;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(428, 249);
+            label9.Name = "label9";
+            label9.Size = new Size(38, 15);
+            label9.TabIndex = 17;
+            label9.Text = "label9";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(422, 281);
+            label10.Name = "label10";
+            label10.Size = new Size(44, 15);
+            label10.TabIndex = 18;
+            label10.Text = "label10";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(textBox5);
+            Controls.Add(label7);
+            Controls.Add(textBox4);
+            Controls.Add(label8);
+            Controls.Add(button4);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
+            Controls.Add(label6);
+            Controls.Add(button3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label4);
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
+        private Button button2;
+        private Label label5;
+        private TextBox textBox3;
+        private Label label6;
+        private Button button3;
+        private Label label7;
+        private TextBox textBox4;
+        private Label label8;
+        private Button button4;
+        private TextBox textBox5;
+        private Label label9;
+        private Label label10;
+    }
 }
--- lab2/Form1.cs
+++ lab2/Form1.cs
@@ -1,10 +1,72 @@
 namespace lab2
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int RecursiveFactorial(int n)
+        {
+            if (n == 0) return 1;
+            return n * RecursiveFactorial(n - 1);
+        }
+
+        private void btnCalculateFactorial_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(txtInput.Text);
+            int result = RecursiveFactorial(number);
+            lblResult.Text = $"Factorial: {result}";
+        }
+
+        public int RecursiveSum(int[] numbers, int n)
+        {
+            if (n == 0) return numbers[0];
+            return numbers[n] + RecursiveSum(numbers, n - 1);
+        }
+
+        private void btnCalculateSum_Click(object sender, EventArgs e)
+        {
+            int[] numbers = txtArrayInput.Text.Split(',').Select(int.Parse).ToArray();
+            int result = RecursiveSum(numbers, numbers.Length - 1);
+            lblSumResult.Text = $"Sum: {result}";
+        }
+        public int RecursiveFibonacci(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+            return RecursiveFibonacci(n - 1) + RecursiveFibonacci(n - 2);
+        }
+        private void FiboCal_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(FiboInput.Text);
+            int result = RecursiveFibonacci(number);
+            lblFiboResult.Text = $"F = {result}";
+        }
+
+        public int RecursivePower(int n, int power)
+        {
+            if (power == 0) return 1;
+            return n * RecursivePower(n, power - 1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int baseNum = int.Parse(inputx.Text);
+            int exponent = int.Parse(inputpower.Text);
+            int result = RecursivePower(baseNum, exponent);
+            lblPowerResult.Text = $"Result: {result}";
+        }
+
+        private void btnPowerCalculate_Click(object sender, EventArgs e)
+        {
+            int baseNum = int.Parse(inputx.Text);
+            int exponent = int.Parse(inputpower.Text);
+            int result = RecursivePower(baseNum, exponent);
+            lblPowerResult.Text = $"Power: {result}";
+        }
+    }
 }
